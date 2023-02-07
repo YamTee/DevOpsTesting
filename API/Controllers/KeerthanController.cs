186b114777ce02e859e52387a5b34ba57f98f6ba
Mohammed Thalha <mohammed.t@ibridgellc.com>
2023-02-07 17:15:06 +0530

    Add project files.

---
+++ API/Controllers/KeerthanController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class KeerthanController : Controller
+    {
+        [Route("/api/keerthan")]
+        public IActionResult Index()
+        {
+            return Json(new
+            {
+                name = "Keerthan",
+                team = "Cloud Team"
+            });
+        }
+    }
+}
---
+++ API/Controllers/PrashanthController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class PrashanthController : Controller
+    {
+        [Route("/api/prashanth")]
+        public IActionResult Index()
+        {
+            return Json(new
+            {
+                name = "Prashanth",
+                team = "Cloud Team"
+            });
+        }
+    }
+}
